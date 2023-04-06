cddfce2ed920d740a187cf9e77a70c6e92c13d2e
seohoon <wnsj20@gmail.com>
2023-04-06 16:16:58 +0900

    04_06 06) commit

--- src/main/java/com/shukream/event/controller/EventController.java
+++ src/main/java/com/shukream/event/controller/EventController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 // 컨트롤러로 사용하기 위한 Bean 생성
@@ -67,11 +68,37 @@ public class EventController {
   // #3 ) 2차 주소 : http://localhost:8090/teamproject/event/detailresult.do
   // 가져오는 방식 : GET 방식으로 적용 
   @RequestMapping(value = "/detailresult.do", method = RequestMethod.GET)
-  public ModelAndView detailresult(HttpServletRequest request, HttpServletResponse response) {
+  public ModelAndView detailresult(@RequestParam("ticket")String ticket,HttpServletRequest request, HttpServletResponse response) {
 	  
 	  
 	// ModelANdView 객체 생성
     ModelAndView mav = new ModelAndView();
+    
+    // eventdetail.jsp에서 location.replace로 전달받은 값을 가져온다.
+    // 1) 추가중 : login 구현 후 session id 가져오기,
+    // 1-1) 무료배송권 : freeshipping
+    // 1-2) 30일무료보관권 : freestore
+    // 1-3) 수수료 1회 면제권 : freecharge
+    // 1-4) 꽝 : OTL
+    System.out.println(ticket);
+    
+    // ticket 내용에 따라서 각각 다른 메소드를 호출 시킨다.
+    if(ticket.equals("freeshipping")){
+    	
+    	
+    	
+    	
+    }else if(ticket.equals("freestore")) {
+    	
+    	
+    }else if(ticket.equals("freecharge")) {
+    	
+    	
+    }else {
+    	
+    	
+    	
+    }
     
     // Viewname 가져오기
     String viewName = (String) request.getAttribute("viewName");
@@ -85,6 +112,8 @@ public class EventController {
     // ModelAndView 반환
     return mav;
   }
+  
+
   
   
 
